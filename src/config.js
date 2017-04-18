14acd00dd89d54ef59d7766f0b128a62ac7ac74e
Unknown <r.chase.weller@gmail.com>
2017-04-18 13:00:00 -0600

    Added a feeback page | connected past entries

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
-export default {
+import Rebase from 're-base';
+
+export const base = Rebase.createClass({
   apiKey: "AIzaSyAJCfOD-ESXHmnuYhe9AlvS3JoWH6UJFzo",
   authDomain: "emotion-tracker-fc3ab.firebaseapp.com",
   databaseURL: "https://emotion-tracker-fc3ab.firebaseio.com",
   storageBucket: "emotion-tracker-fc3ab.appspot.com",
   messagingSenderId: "635552038798"
-};
+});
 
 export const defaultConfig = {
   userProfile: null,
@@ -20,17 +22,3 @@ export const supportedAuthProviders = [
   'google',
   'facebook'
 ]
-
-
-// /** @constant
-//  * @description Default initial props used when running firebase.initializeApp
-//  * @type {Array}
-//  * @private
-//  */
-// export const defaultInitProps = [
-//   'apiKey',
-//   'authDomain',
-//   'databaseURL',
-//   'storageBucket',
-//   'messagingSenderId'
-// ]
---
+++ src/routes/Feedback/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by chaseweller on 4/17/17.
+ */
